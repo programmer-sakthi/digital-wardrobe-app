1100948ea4b2dffbae6b99e3894f23d941f67471
sakthi <sakthipalanisamy7@gmail.com>
2024-09-03 07:14:06 +0530

    Signup page added

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import classes from "./Signup.module.css";
 
 function Signup() {
   return (
-    <div>
-        <h1 style={{color : "White"}}> Signup</h1>
+    <div className={classes.container}>
+      <div className={classes.signup}>
+        <h1>Signup</h1>
+        <div className={classes.div}>
+          <label className={classes.label}>Name</label>
+          <input
+            type="text"
+            placeholder="username"
+            className={classes.input}
+          />
+        </div>
+        <div className={classes.div}>
+          <label className={classes.label}>UserName</label>
+          <input
+            type="text"
+            placeholder="username"
+            className={classes.input}
+          />
+        </div>
+        <div className={classes.div}>
+          <label className={classes.label}>Email</label>
+          <input
+            type="email"
+            placeholder="username"
+            className={classes.input}
+          />
+        </div>
+        <div className={classes.div}>
+          <label className={classes.label}>Password</label>
+          <input
+            type="password"
+            placeholder="username"
+            className={classes.input}
+          />
+        </div>
+        <button className={classes.button}>SignUp</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
