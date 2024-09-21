ef63888dce24657c51b3dfd7f81137b18dadf679
sakthi <sakthipalanisamy7@gmail.com>
2024-09-21 09:51:24 +0530

    Login page redirects only if user email is verified

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,9 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { auth } from "../../config/firebase";
 import classes from "./Login.module.css";
-import { toast } from "react-toastify";
 
 function Login() {
   const navigate = useNavigate();
@@ -13,11 +13,16 @@ function Login() {
   const handleLogin = () => {
     signInWithEmailAndPassword(auth, username, password)
       .then((userCredential) => {
-        navigate("./all-dresses");
-        toast.success("User logged in succesfully")
+        if (userCredential.user.emailVerified) {
+          navigate("./all-dresses");
+          toast.success("User logged in succesfully");
+        } else {
+          toast.error("Email not verified");
+          toast.info("check your mail for verification link");
+        }
       })
       .catch((error) => {
-        toast.error(error.message)
+        toast.error(error.message);
         console.log(error);
       });
   };
@@ -33,8 +38,8 @@ function Login() {
             type="email"
             placeholder="username"
             className={classes.loginInput}
-            onChange={ (e) => setUserName(e.target.value) }
-            />
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </div>
         <div className={classes.loginDiv}>
           <label className={classes.loginLabel}>Password</label>
@@ -43,7 +48,7 @@ function Login() {
             type="text"
             placeholder="password"
             className={classes.loginInput}
-            onChange={ (e) => setPassword(e.target.value) }
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className={classes.forgotPassword}>
