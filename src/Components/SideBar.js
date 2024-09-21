91a18ac545b2579b5af6c1c831250ca92f051502
sakthi <sakthipalanisamy7@gmail.com>
2024-09-21 07:41:39 +0530

    Log out functionality added

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import classes from './SideBar.module.css';
+import { signOut } from "firebase/auth";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { auth } from "../config/firebase";
+import classes from "./SideBar.module.css";
 
 function SideBar() {
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    await signOut(auth);
+    toast.info("User logged out successfully !");
+    navigate("/");
+  };
+
   return (
     <div className={classes.sideBar}>
-        <button>Log out</button>
-        <button>Profile</button>
+      <button onClick={handleLogOut}>Log out</button>
+      <button>Profile</button>
     </div>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
